573a447caf1f1d9d806b3c9df4cfefcc445da01d
jijihorang <jiho7213@gmail.com>
2025-03-19 16:55:13 +0900

    [init]starvive create project

---
+++ src/main/java/com/starbucks/starvive/StarviveApplication.java
@@ -0,0 +1,13 @@
+package com.starbucks.starvive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarviveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarviveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starbucks/starvive/StarviveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starbucks.starvive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarviveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
